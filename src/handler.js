c0212ec36a46d42e683fbf771b3698aa39836452
deknusapt <dev.deknusa@gmail.com>
2024-05-05 17:22:00 +0800

    Adding getAllBooksHandler() and it's route with details

--- src/handler.js
+++ src/handler.js
@@ -71,4 +71,55 @@ const addBookHandler = (request, h) => {
   return response.code(500);
 };
 
-module.exports = { addBookHandler };
+const getAllBooksHandler = (request, h) => {
+  const { name, reading, finished } = request.query;
+
+  let keywordBooks = books;
+
+  if (name !== undefined) {
+    keywordBooks = keywordBooks.filter((book) => book
+      .name.toLowerCase().includes(name.toLowerCase()));
+  }
+  if (reading !== undefined) {
+    keywordBooks = keywordBooks.filter((book) => book.reading === !Number(reading));
+  }
+
+  if (finished !== undefined) {
+    keywordBooks = keywordBooks.filter((book) => book.finished === !Number(finished));
+  }
+
+  const response = h.response({
+    status: 'success',
+    data: {
+      books: keywordBooks.map((book) => ({
+        id: book.id,
+        name: book.name,
+        publisher: book.publisher,
+      })),
+    },
+  });
+  return response.code(200);
+};
+
+const getAllBooksByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const book = books.filter((n) => n.id === id)[0];
+
+  if (book !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        book,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan',
+  });
+  return response.code(404);
+};
+
+module.exports = { addBookHandler, getAllBooksHandler, getAllBooksByIdHandler };
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,24 @@
-const { addBookHandler } = require('./handler');
+const {
+  addBookHandler,
+  getAllBooksHandler,
+  getAllBooksByIdHandler,
+} = require('./handler');
 
 const routes = [
   {
     method: 'POST',
     path: '/books',
     handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getAllBooksByIdHandler,
   },
 ];
 
